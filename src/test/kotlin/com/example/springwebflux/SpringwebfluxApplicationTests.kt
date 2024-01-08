57e2bffc97fdb2b18e7469ea5838e942ff070d9b
cherhy <ekxk1234@naver.com>
2024-01-09 00:28:10 +0900

    create project

---
+++ src/test/kotlin/com/example/springwebflux/SpringwebfluxApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.example.springwebflux
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringwebfluxApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
