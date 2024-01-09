a9967ddc0519b06eb23c81982888fbd28196faa2
cherhy <ekxk1234@naver.com>
2024-01-10 00:00:48 +0900

    create business logic

---
+++ src/main/kotlin/com/example/springwebflux/BookService.kt
@@ -0,0 +1,52 @@
+package com.example.springwebflux
+
+import org.springframework.stereotype.Service
+import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toFlux
+import reactor.kotlin.core.publisher.toMono
+import java.util.concurrent.atomic.AtomicInteger
+
+@Service
+class BookService {
+
+    private final val nextId = AtomicInteger(0)
+
+    val books = mutableListOf(
+        Book(id = nextId.incrementAndGet(), name = "코틀린 인 액션", price = 30000),
+        Book(id = nextId.incrementAndGet(), name = "HTTP 완벽 가이드", price = 40000),
+        Book(id = nextId.incrementAndGet(), name = "이펙티브 자바", price = 35000),
+    )
+
+    fun getAll() = books.toFlux()
+
+    fun get(id: Int) =
+        books.find { it.id == id }
+            .toMono()
+
+    fun add(request: Map<String, Any>) =
+        Mono.just(request)
+            .map { map ->
+                val book = Book(
+                    id = nextId.incrementAndGet(),
+                    name = map["name"].toString(),
+                    price = map["price"] as Int,
+                )
+
+                books.add(book)
+                book
+            }
+
+    fun delete(id: Int) =
+        books.removeIf {
+            it.id == id
+        }
+            .toMono()
+            .then()
+
+}
+
+data class Book(
+    val id: Int,
+    val name: String,
+    val price: Int,
+)
