ed0485e3c185cdabf9d7b6b3f6645ccace7bd876
cherhy <ekxk1234@naver.com>
2024-01-10 00:00:32 +0900

    create rest controller

---
+++ src/main/kotlin/com/example/springwebflux/BookController.kt
@@ -0,0 +1,27 @@
+package com.example.springwebflux
+
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class BookController(
+    private val bookService: BookService,
+) {
+
+    @GetMapping("/books")
+    fun getAll() = bookService.getAll()
+
+    @GetMapping("/books/{id}")
+    fun get(@PathVariable id: Int) = bookService.get(id)
+
+    @PostMapping("/books")
+    fun add(@RequestBody request: Map<String, Any>) = bookService.add(request)
+
+    @DeleteMapping("/books/{id}")
+    fun delete(@PathVariable id: Int) = bookService.delete(id)
+
+}
