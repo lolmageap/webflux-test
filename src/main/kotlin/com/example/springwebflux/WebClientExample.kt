d6eab5f8cf1c25a1a4b9848f5fc401beca7b9c8c
cherhy <ekxk1234@naver.com>
2024-01-10 00:02:14 +0900

    create rest template and webclient

--- src/main/kotlin/com/example/springwebflux/WebClientExample.kt
+++ src/main/kotlin/com/example/springwebflux/WebClientExample.kt
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.bodyToFlux
 import reactor.core.publisher.Flux
 
 @RestController
